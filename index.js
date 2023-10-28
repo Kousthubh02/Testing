b48489ccb14ab0baa780aacd2fd5f0a0898488dd
Prashant Patil <109451910+Prash1001@users.noreply.github.com>
2023-10-28 12:16:39 +0530

    query for display to page added

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-const express = require('express')
-const cors=require('cors');
-const mysql = require('mysql');
-const app=express();
-const port=5000;
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql2/promise'); 
 const fileUpload = require('express-fileupload');
+const app = express();
+const port = 8000;
 app.use(fileUpload());
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-app.use('/',require('./routes/formSubmit'))
+// Replace your database connection configuration
+const pool = require('./db'); 
+
+app.get('/',async (_req,res) => {
+console.log("you are conneted to this port");
+});
 
 
-app.listen(port,()=>{
-    console.log(`Listening to port ${port}`);
-})
+app.get('/personal_details', async (_req, res) => {
+  try {
+    const [rows, fields] = await pool.query('SELECT * FROM personal_details');
+    res.json({ data: rows });
+  } catch (error) {
+    console.error('Error executing query:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
+
+
+app.use('/',require('./routes/formSubmit'));
+
+
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+});
