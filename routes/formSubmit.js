ec0dbce6bf71fbb4a45a430aa2afe7d74d0c6dc9
Kousthubh Yadavalli <kyadavalli04@gmail.com>
2023-12-11 21:41:02 +0530

    bug detected

--- routes/formSubmit.js
+++ routes/formSubmit.js
@@ -1,34 +1,18 @@
-const mysql = require("mysql");
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator");
-const connection = require("../db");
 const multer = require('multer');
-
+const upload=multer({dest:'uploads/'})
 
 // endpoint for profile image
 router.get("/profile", (req, res) => {
   res.json({ message: "profile form active" });
 });
 
-const storage=multer.diskStorage({
-  destination:function(req,file,cb){
-    return cb(null,"./files")
-  },
-  filename:function(req,file,cb){
-    return cb(null,`${Date.now()}-${file.originalname}`)
-  }
-});
-const upload=multer({storage:storage})
 
 
 
 router.post("/profile", upload.single('file'), (req, res) => {
-  console.log(req.files);
-
-  if (!req.files) {
-    return res.status(400).json({ error: "No file uploaded" });
-  }    
+  console.log(req.files);  
     res.json({ message: "File uploaded successfully"});
   });
 
